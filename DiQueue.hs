bed270ba7e5b3c3d4bafe5eac564574c602b41bd
Dennis Griffith <isanobody@gmail.com>
2015-08-19 16:49:57 -0500

    split global diqueue lock into two

--- DiQueue.hs
+++ DiQueue.hs
@@ -4,70 +4,86 @@
 -- The implementation is via the normal pair of lists where the writing end is locked
 module DiQueue where
 import Control.Concurrent
+import Data.IORef
 
 -- DiQueues have a direction, represented at the type level by the following type. ToX
 -- refers to who can read
 
 data DiQueueCore :: * -> * where
-  ToC :: [a] -> DiQueueCore a
-  ToP :: [a] -> DiQueueCore a
+  ToC :: MVar [a] -> MVar [a] -> DiQueueCore a
+  ToP :: MVar [a] -> MVar [a] -> DiQueueCore a
 
-type DiQueue a = MVar (DiQueueCore a)
+type DiQueue a = IORef (DiQueueCore a)
 
 -- Bool indicates to start pointing at Client or Provider
 newDiQueue :: Bool -> IO (DiQueue a)
-newDiQueue dir = if dir
-                 then newMVar (ToC [])
-                 else newMVar (ToP [])
+newDiQueue dir = do h <- newMVar []
+                    t <- newMVar []
+                    if dir
+                    then newIORef (ToC h t)
+                    else newIORef (ToP h t)
 
 safeReadC :: DiQueue a -> IO a
 safeReadC qr =
- do q <- takeMVar qr
+ do q <- readIORef qr
     case q of
-      ToP l -> putMVar qr (ToP l) >> yield >> safeReadC qr
-      ToC [] -> putMVar qr (ToC []) >> yield >> safeReadC qr
-      ToC (x:xs) -> putMVar qr (ToC xs) >> yield >> return x
+      ToP _ _ -> yield >> safeReadC qr
+      ToC h t -> do xss <- takeMVar h
+                    case xss of
+                      [] -> do ys <- takeMVar t
+                               putMVar t []
+                               putMVar h (reverse ys)
+                               yield
+                               safeReadC qr
+                      (x:xs) -> do putMVar h xs
+                                   return x
 
 safeReadP :: DiQueue a -> IO a
 safeReadP qr =
- do q <- takeMVar qr
+ do q <- readIORef qr
     case q of
-      ToC l -> putMVar qr (ToC l) >> yield >> safeReadP qr
-      ToP [] -> putMVar qr (ToP []) >> yield >> safeReadP qr
-      ToP (x:xs) -> putMVar qr (ToP xs) >> yield >> return x
+      ToC _ _ -> yield >> safeReadP qr
+      ToP h t -> do xss <- takeMVar h
+                    case xss of
+                      [] -> do ys <- takeMVar t
+                               putMVar t []
+                               putMVar h (reverse ys)
+                               yield
+                               safeReadP qr
+                      (x:xs) -> do putMVar h xs
+                                   return x
 
 safeWriteC :: DiQueue a -> a -> IO ()
 safeWriteC qr x = 
-  do q <- takeMVar qr
+  do q <- readIORef qr
      case q of
-       ToP l -> putMVar qr (ToP (l++[x]))
-       ToC l -> putMVar qr (ToC l) >> yield >> safeWriteC qr x
+       ToP _ t -> do xs <- takeMVar t
+                     putMVar t (x:xs)
+       ToC _ _ -> yield >> safeWriteC qr x
 
 safeWriteP :: DiQueue a -> a -> IO ()
 safeWriteP qr x = 
-  do q <- takeMVar qr
+  do q <- readIORef qr
      case q of
-       ToC l -> putMVar qr (ToC (l++[x]))
-       ToP l -> putMVar qr (ToP l) >> yield >> safeWriteP qr x
+       ToC _ t -> do xs <- takeMVar t
+                     putMVar t (x:xs)
+       ToP _ _ -> yield >> safeWriteP qr x
 
 
 waitToC :: DiQueue a -> IO ()
-waitToC qr = do q <- takeMVar qr
+waitToC qr = do q <- readIORef qr
                 case q of
-                  ToC l -> putMVar qr (ToC l)
-                  ToP l -> putMVar qr (ToP l) >> yield >> waitToC qr
+                  ToC _ _ -> return ()
+                  ToP _ _ -> yield >> waitToC qr
 
 waitToP :: DiQueue a -> IO ()
-waitToP qr = do q <- takeMVar qr
+waitToP qr = do q <- readIORef qr
                 case q of
-                  ToP l -> putMVar qr (ToP l)
-                  ToC l -> putMVar qr (ToC l) >> yield >> waitToP qr
+                  ToP _ _ -> return ()
+                  ToC _ _ -> yield >> waitToP qr
 
--- TODO get rid of this error checking
+-- TODO Add assertions?
 swapDir :: DiQueue a -> IO ()
-swapDir qr = 
-  do q <- takeMVar qr
-     case q of
-       ToC [] -> putMVar qr (ToP [])
-       ToP [] -> putMVar qr (ToC [])
-       _ -> error "swapDir of non-empty queue"
+swapDir qr = atomicModifyIORef' qr (\q -> case q of
+       ToC h t -> (ToP h t,())
+       ToP h t -> (ToC h t,()))
