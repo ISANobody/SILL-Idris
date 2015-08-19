61cec5b9d5fe31013c524c7a4df4824209cc632f
Dennis Griffith <isanobody@gmail.com>
2015-08-19 17:22:18 -0500

    DiQueue with one Chan

--- DiQueue.hs
+++ DiQueue.hs
@@ -9,71 +9,63 @@ import Data.IORef
 -- DiQueues have a direction, represented at the type level by the following type. ToX
 -- refers to who can read
 
-data DiQueueCore :: * -> * where
-  ToC :: IORef [a] -> Chan a -> DiQueueCore a
-  ToP :: IORef [a] -> Chan a -> DiQueueCore a
+data Dir = ToC | ToP
+
+data DiQueueCore a = DQC { dir :: Dir
+                         , que :: Chan a }
 
 type DiQueue a = IORef (DiQueueCore a)
 
 -- Bool indicates to start pointing at Client or Provider
 newDiQueue :: Bool -> IO (DiQueue a)
-newDiQueue dir = do h <- newIORef []
-                    t <- newChan
-                    if dir
-                    then newIORef (ToC h t)
-                    else newIORef (ToP h t)
+newDiQueue d = do q <- newChan
+                  if d
+                  then newIORef (DQC ToC q)
+                  else newIORef (DQC ToP q)
 
 safeReadC :: DiQueue a -> IO a
 safeReadC qr =
  do q <- readIORef qr
-    case q of
-      ToP _ _ -> yield >> safeReadC qr
-      ToC h t -> do xss <- readIORef h
-                    case xss of
-                      [] -> readChan t
-                      (x:xs) -> do writeIORef h xs
-                                   return x
+    case dir q of
+      ToP -> yield >> safeReadC qr
+      ToC -> readChan (que q)
 
 safeReadP :: DiQueue a -> IO a
 safeReadP qr =
  do q <- readIORef qr
-    case q of
-      ToC _ _ -> yield >> safeReadP qr
-      ToP h t -> do xss <- readIORef h
-                    case xss of
-                      [] -> do readChan t
-                      (x:xs) -> do writeIORef h xs
-                                   return x
+    case dir q of
+      ToC -> yield >> safeReadP qr
+      ToP -> readChan (que q)
 
 safeWriteC :: DiQueue a -> a -> IO ()
 safeWriteC qr x = 
   do q <- readIORef qr
-     case q of
-       ToP _ t -> writeChan t x
-       ToC _ _ -> yield >> safeWriteC qr x
+     case dir q of
+       ToP -> writeChan (que q) x
+       ToC -> yield >> safeWriteC qr x
 
 safeWriteP :: DiQueue a -> a -> IO ()
 safeWriteP qr x = 
   do q <- readIORef qr
-     case q of
-       ToC _ t -> writeChan t x
-       ToP _ _ -> yield >> safeWriteP qr x
+     case dir q of
+       ToC -> writeChan (que q) x
+       ToP -> yield >> safeWriteP qr x
 
 
 waitToC :: DiQueue a -> IO ()
 waitToC qr = do q <- readIORef qr
-                case q of
-                  ToC _ _ -> return ()
-                  ToP _ _ -> yield >> waitToC qr
+                case dir q of
+                  ToC -> return ()
+                  ToP -> yield >> waitToC qr
 
 waitToP :: DiQueue a -> IO ()
 waitToP qr = do q <- readIORef qr
-                case q of
-                  ToP _ _ -> return ()
-                  ToC _ _ -> yield >> waitToP qr
+                case dir q of
+                  ToP -> return ()
+                  ToC -> yield >> waitToP qr
 
 -- TODO Add assertions?
 swapDir :: DiQueue a -> IO ()
-swapDir qr = atomicModifyIORef' qr (\q -> case q of
-       ToC h t -> (ToP h t,())
-       ToP h t -> (ToC h t,()))
+swapDir qr = atomicModifyIORef' qr (\q -> case dir q of
+       ToC -> (q{dir = ToP},())
+       ToP -> (q{dir = ToC},()))
