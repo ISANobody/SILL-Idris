b8f56e09c612ed6a93896a6deed526c0847f4c6a
Dennis Griffith <isanobody@gmail.com>
2015-08-19 18:23:15 -0500

    use finer grained STM for DiQueue

--- DiQueue.hs
+++ DiQueue.hs
@@ -3,69 +3,52 @@
 
 -- The implementation is via the normal pair of lists where the writing end is locked
 module DiQueue where
-import Control.Concurrent
 import Control.Concurrent.STM
 
 -- DiQueues have a direction, represented at the type level by the following type. ToX
 -- refers to who can read
 
-data Dir = ToC | ToP
+data Dir = ToC | ToP deriving (Eq, Show)
 
-data DiQueueCore a = DQC { dir :: Dir
-                         , que :: TChan a }
+invertDir :: Dir -> Dir
+invertDir ToC = ToP
+invertDir ToP = ToC
 
-type DiQueue a = TVar (DiQueueCore a)
+data DiQueue a = DQ { dir :: TVar Dir
+                    , que :: TQueue a }
+
+
+-- Wait until the queue is pointing the specified direction and then modify the Chan
+withDir :: Dir -> (TQueue a -> STM b) -> DiQueue a -> STM b
+withDir d f q = do qd <- readTVar (dir q)
+                   if d == qd 
+                   then f (que q)
+                   else retry
 
 -- Bool indicates to start pointing at Client or Provider
 newDiQueue :: Bool -> IO (DiQueue a)
-newDiQueue d = do q <- newTChanIO
-                  if d
-                  then newTVarIO (DQC ToC q)
-                  else newTVarIO (DQC ToP q)
+newDiQueue b = do q <- newTQueueIO
+                  d <- newTVarIO (if b then ToC else ToP)
+                  return (DQ d q)
 
 safeReadC :: DiQueue a -> STM a
-safeReadC qr =
- do q <- readTVar qr
-    case dir q of
-      ToP -> retry
-      ToC -> readTChan (que q)
+safeReadC = withDir ToC readTQueue
 
 safeReadP :: DiQueue a -> STM a
-safeReadP qr =
- do q <- readTVar qr
-    case dir q of
-      ToC -> retry
-      ToP -> readTChan (que q)
+safeReadP = withDir ToP readTQueue
 
 safeWriteC :: DiQueue a -> a -> STM ()
-safeWriteC qr x = 
-  do q <- readTVar qr
-     case dir q of
-       ToP -> writeTChan (que q) x
-       ToC -> retry
+safeWriteC qr x = withDir ToP (flip writeTQueue $ x) qr
 
 safeWriteP :: DiQueue a -> a -> STM ()
-safeWriteP qr x = 
-  do q <- readTVar qr
-     case dir q of
-       ToC -> writeTChan (que q) x
-       ToP -> retry
-
+safeWriteP qr x = withDir ToC (flip writeTQueue $ x) qr
 
 waitToC :: DiQueue a -> STM ()
-waitToC qr = do q <- readTVar qr
-                case dir q of
-                  ToC -> return ()
-                  ToP -> retry
+waitToC = withDir ToC (\_ -> return ())
 
 waitToP :: DiQueue a -> STM ()
-waitToP qr = do q <- readTVar qr
-                case dir q of
-                  ToP -> return ()
-                  ToC -> retry
+waitToP = withDir ToP (\_ -> return ())
 
 -- TODO Add assertions?
 swapDir :: DiQueue a -> STM ()
-swapDir qr = modifyTVar qr (\q -> case dir q of
-       ToC -> q{dir = ToP}
-       ToP -> q{dir = ToC})
+swapDir qr = modifyTVar' (dir qr) invertDir
